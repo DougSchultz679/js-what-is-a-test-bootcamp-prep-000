a0140bb980aa2254831d054f3064d1c14595dd23
Douglas W Schultz <vxpx17@gmail.com>
2017-11-25 22:22:18 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -9,7 +9,7 @@ describe('what-is-a-test', () => {
 
 
   describe('Height', () => {
-    it('is less than 40', () => {
+    it('is less than 200', () => {
       expect(index.height).toBeLessThan(200)
     })
   })
